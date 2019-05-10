ea7f86c42912787b1b3361f908a5b9e266faa2b0
Nathan Myers <nathan.myers@steelseries.com>
2019-05-10 11:44:17 -0500

    adding routing

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from "react"
+import ReactDOM from "react-dom"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
 
-class HelloMessage extends React.Component {
-  render() {
-    return <div>Hello {this.props.name}</div>;
-  }
+import Home from './containers/Home'
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+            <li>
+              <Link to="/users/">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Home} />
+      </div>
+    </Router>
+  )
 }
 
 var mountNode = document.getElementById("app");
-ReactDOM.render(<HelloMessage name="Jane" />, mountNode);
+ReactDOM.render(<App />, mountNode);
